5484aff5d8127d7958c899b9fe4e9d44f2dd2c87
longavailable <>
2023-04-04 13:21:22 +0800

    Update schedulely

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ curl -s myip.ipip.net -o ip.txt && sed -i -e 's/[^0-9.]//g' ip.txt
 git config user.name 'longavailable'
 git config user.email '<>'
 git add .
-git diff-index --quiet HEAD || git commit -m 'Update schedulely' && git push origin
+git diff-index --quiet HEAD || git commit -m 'Update schedulely' && git push origin main
