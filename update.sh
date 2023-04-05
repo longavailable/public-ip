a3e056c96416790eb94fabcf97383d1308eb34bf
Xiaolong "Bruce" Liu <liuxiaolong125@gmail.com>
2023-04-05 09:26:04 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,5 @@ git pull
 
 curl -s myip.ipip.net -o ip.txt && sed -i -e 's/[^0-9.]//g' ip.txt
 
-git config user.name 'longavailable'
-git config user.email '<>'
 git add .
 git diff-index --quiet HEAD || git commit -m 'Update schedulely' && git push origin main
